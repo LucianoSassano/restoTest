341fd0d33480e257c72c4de73d4f1ec8b6d24b92
Luciano_Sassano <lucianosassano1@gmail.com>
2020-02-10 09:22:40 -0300

    first commit

---
+++ js/tests.js
@@ -0,0 +1,8 @@
+const expect = chai.expect;
+
+describe('Test funcion reservarHorario,elimina horario que se le pasa',(restaurant,horario) => {
+    let arrayHorarios = restaurant.horarios;
+    let horario = horario;
+
+})
+
